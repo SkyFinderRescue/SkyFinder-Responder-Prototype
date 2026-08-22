3f5f388a9c29426b5b86728625afd22158642092
SkyFinderRescue <sky.bonillo@gmail.com>
2026-08-21 22:08:41 -0700

    Connect isolated Firebase project

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Isolated responder-sharing prototype only. Not used by production SkyFinder.
+// Firebase web configuration values identify the Firebase project; access control is enforced by Firebase Auth + RTDB Security Rules.
+export const firebaseConfig={
+  apiKey:'AIzaSyBFWxwnzKOzD2bOSEOef4ridUAjtudJ4h4M',
+  authDomain:'skyfinder-live-prototype-63e49.firebaseapp.com',
+  databaseURL:'https://skyfinder-live-prototype-63e49-default-rtdb.firebaseio.com',
+  projectId:'skyfinder-live-prototype-63e49',
+  storageBucket:'skyfinder-live-prototype-63e49.firebasestorage.app',
+  messagingSenderId:'736184546851',
+  appId:'1:736184546851:web:7bc0851ac292cd4b876396'
+};
